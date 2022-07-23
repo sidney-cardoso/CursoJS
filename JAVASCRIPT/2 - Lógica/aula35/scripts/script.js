c2738ea0bb46951e13f8843bb0c1bd6e243bbd97
Sidney Cardoso <sidneyrsc342@gmail.com>
2022-07-23 12:37:19 -0300

    .

--- JAVASCRIPT/2 - Lógica/aula35/scripts/script.js
+++ JAVASCRIPT/2 - Lógica/aula35/scripts/script.js
@@ -4,3 +4,17 @@ const elements = [
     { tag: 'footer', text: 'This is a footer' },
     { tag: 'section', text: 'This is a section' }
 ]
+
+
+
+
+
+// ------------------------------------------------- MINHA SOLUÇÃO ------------------------------------------------- //
+/* 
+for (let i = 0; i < elements.length; i++) {
+    const container = document.querySelector('.container')
+    const element = document.createElement(elements[i].tag)
+    element.innerHTML = elements[i].text
+    container.appendChild(element)
+}
+ */
